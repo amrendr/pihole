425e8190f5ce6a4a8b77ee7e968032a3b1a2fbb4
amrendr <amrendra@gmail.com>
2023-03-19 18:24:50 -0400

    Update youtube.sh

--- youtube/youtube.sh
+++ youtube/youtube.sh
@@ -14,25 +14,35 @@ sudo curl 'https://raw.githubusercontent.com/kboghdady/youTube_ads_4_pi-hole/mas
 sudo curl 'https://raw.githubusercontent.com/amrendr/pihole/main/youtube/ignore.list' >> $ignorelist
 
 
+echo 'Create list from FTL db'
 sudo /usr/bin/sqlite3 /etc/pihole/pihole-FTL.db "select domain from queries where domain like '%googlevideo.com'" > $tempblacklist
+
+echo 'Create list from blocked domain list'
 sudo /usr/bin/sqlite3 /etc/pihole/gravity.db "select domain from domainlist where domain like '%googlevideo.com'" > $blacklist
 
+
 # Remove duplicates
+echo 'Remove duplicates'
 gawk -i inplace '!a[$0]++' $tempblacklist
 gawk -i inplace '!a[$0]++' $blacklist
 gawk -i inplace '!a[$0]++' $ignorelist
 
 # Remove already blocked domain from templist
+echo 'Remove adlist from potential blacklist'
 while read line ;  do  sed -i "/.*$line.*/d" $tempblacklist ; done < $adlist
+echo 'Remove existing blacklist from potential blacklist'
 while read line ;  do  sed -i "/.*$line.*/d" $tempblacklist ; done < $blacklist
 
+echo 'Remove ignored list from potential blacklist'
 # remove the domains from the ignore.list 
 while read line ;  do  sed -i "/.*$line.*/d" $tempblacklist ; done < $ignorelist
 
+echo 'Remove ignored list from existing blacklist'
 # this in case you have an old blocked domain the the database 
 while read ignoredDns ; do sudo /usr/bin/sqlite3 /etc/pihole/gravity.db "delete from domainlist where domain like '%$ignoredDns%' " ; done < $ignorelist
 
 
+echo 'add to blacklist domain'
 ## adding it to the blacklist in Pihole V5 
 # only 200 Domains at once
 sudo xargs -a $tempblacklist -L200 pihole -b -nr
